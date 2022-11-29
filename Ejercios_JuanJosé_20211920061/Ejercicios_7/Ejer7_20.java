c57a091b2243fd5b0489dcae1ccc88caeaad8fcf
Croximity <106570269+Croximity@users.noreply.github.com>
2022-11-28 23:28:55 -0600

    Add files via upload

---
+++ Ejercios_JuanJosé_20211920061/Ejercicios_7/Ejer7_20.java
@@ -0,0 +1,24 @@
+public class Ejer7_20 {
+    //Array inverso utilizando Temp
+  
+    static void reverse(int a[], int n)
+    {
+        int[] b = new int[n];
+        int j = n;
+        for (int i = 0; i < n; i++) {
+            b[j - 1] = a[i];
+            j = j - 1;
+        }
+
+        System.out.println("\n     El array inverso es: \n");
+        for (int k = 0; k < n; k++) {
+            System.out.println(b[k]);
+        }
+    }
+  
+    public static void main(String[] args)
+    {
+        int [] arr = {10, 20, 30, 40, 50};
+        reverse(arr, arr.length);
+    }
+}
